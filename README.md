900461d163ec3da9537fa76acbc7ead28695cc72
Lupin <lupin@lp1.eu>
2016-10-28 16:06:30 +0200

    updated readme and requirements

---
+++ README.md
@@ -0,0 +1,11 @@
+### Link Poller
+
+## Requirements
+
+- Python3.X
+
+## Usage
+
+```./poller.py --url {http://qndd} --method {GET|POST|PUT|DELETE} --sleeptime {X (in ms)} --log {logfile.log}```
+
+
--- poller.py
+++ poller.py
@@ -1,6 +1,6 @@
 #!/bin/python
 
-from sys import exit
+from sys import exit, argv
 from time import sleep
 from os.path import exists
 import requests
@@ -8,8 +8,9 @@ import json
 
 sleeptime = 60000
 logfile = "/tmp/log.txt"
-url = "https://intra.epitech.eu/module/2016/M-ADS-775/PAR-9-1/register?format=json"
-headers = {"Cookie": "PHPSESSID=0snv902iu8ks0cf7n61b2nad61; _ga=GA1.3.1102579131.1476529245; _gat=1"}
+url = "https://your_url"
+method = "POST"
+headers = {"Cookie": "PHPSESSID=42"}
 last_output = None
 
 def log(data):
@@ -18,7 +19,14 @@ def log(data):
 
 def make_request():
   global last_output
-  r = requests.post(url, headers=headers)
+  if method == "POST":
+    r = requests.post(url, headers=headers)
+  elif method == "GET":
+    r = requests.get(url, headers=headers)
+  elif method == "PUT":
+    r = requests.put(url, headers=headers)
+  elif method == "DELETE":
+    r = requests.delete(url, headers=headers)
   output = {"text":r.text, "status_code":r.status_code}
   if last_output is None:
     last_output = output
@@ -26,8 +34,21 @@ def make_request():
     log(json.dumps(output))
   print(r.status_code)
   print(r.text)
-  
+
+def set_args():
+  global sleeptime, logfile, url, method
+  for i, arg in enumerate(argv):
+    if arg == "--sleeptime" and len(argv) > (i + 1):
+      sleeptime = int(argv[i + 1])
+    if arg == "--log" and len(argv) > (i + 1):
+      logfile = argv[i + 1]
+    if arg == "--url" and len(argv) > (i + 1):
+      url = argv[i + 1]
+    if arg == "--method" and len(argv) > (i + 1):
+      method = argv[i + 1]
+
 def	main():
+  set_args()
   while not exists(logfile):
     make_request()
     sleep(sleeptime)
